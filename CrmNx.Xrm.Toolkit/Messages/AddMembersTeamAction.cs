dab3bcddd4d22f53d855e5cc7d2a19218111087d
ttkoma <k.lapeev@gmail.com>
2020-12-11 04:16:22 +0300

    add AddMembersTeam bound action with initial tests

---
+++ CrmNx.Xrm.Toolkit/Messages/AddMembersTeamAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CrmNx.Xrm.Toolkit.Messages
+{
+    /// <summary>
+    /// Adds members to a team.
+    /// </summary>
+    public class AddMembersTeamAction : WebApiActionBase
+    {
+        private const string ActionName = "Microsoft.Dynamics.CRM.AddMembersTeam";
+        private const string MembersParameterName = "Members";
+
+        public IList<EntityReference> Members => Parameters[MembersParameterName] as IList<EntityReference>;
+
+        /// <summary>
+        /// Create action instance
+        /// </summary>
+        /// <param name="team">The team to which members will be added</param>
+        public AddMembersTeamAction(EntityReference team) : base(team, ActionName)
+        {
+            var membersList = new List<EntityReference>();
+            
+            Parameters.Add(MembersParameterName, membersList);
+        }
+    }
+}
---
+++ Tests/FunctionalTests/Messages/AddMembersTeamActionTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using CrmNx.Crm.Toolkit.Testing.Functional;
+using CrmNx.Xrm.Toolkit.Messages;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CrmNx.Xrm.Toolkit.FunctionalTests.Messages
+{
+    public class AddMembersTeamActionTests: IntegrationTest<TestStartup>
+    {
+        private const string SystemUserEntityName = "systemuser";
+        private const string TeamEntityName = "team";
+        
+        public AddMembersTeamActionTests(TestStartup fixture, ITestOutputHelper outputHelper)
+            : base(fixture, outputHelper)
+        {
+        }
+        
+        [Fact()]
+        public async Task ExecuteAsync_AddMembersTeamAction_When_OneUsersAdded_Then_Ok()
+        {
+            var unitTestsUserId = CrmClient.GetMyCrmUserId();
+
+            var userRef = new EntityReference(SystemUserEntityName, unitTestsUserId);
+            var boundTeamRef = new EntityReference(TeamEntityName, new Guid("a640a425-a463-ea11-aae8-005056b42cd8"));
+
+            var action = new AddMembersTeamAction(boundTeamRef)
+            {
+                Members = { userRef }
+            };
+            
+            await CrmClient.ExecuteAsync(action);
+        }
+        
+        [Fact()]
+        public async Task ExecuteAsync_AddMembersTeamAction_When_MultipleUsersAdded_Then_Ok()
+        {
+            var unitTestsUserId = CrmClient.GetMyCrmUserId();
+            var otherUserId = new Guid("a988c6a2-5236-ea11-aadc-005056b42cd8"); 
+            var testTeamId = new Guid("a640a425-a463-ea11-aae8-005056b42cd8");
+
+            var userRef = new EntityReference(SystemUserEntityName, unitTestsUserId);
+            var userRef1 = new EntityReference(SystemUserEntityName, otherUserId);
+            // var userRef2 = new EntityReference(SystemUserEntityName, unitTestsUserId);
+            var boundTeamRef = new EntityReference(TeamEntityName, testTeamId);
+
+            var action = new AddMembersTeamAction(boundTeamRef)
+            {
+                Members = { userRef, userRef1 }
+            };
+            
+            await CrmClient.ExecuteAsync(action);
+        }
+        
+    }
+}
