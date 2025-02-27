845e0e8a314d6fd0503ed317196ceb87498da280
Konstantin Lapeev <konstantin.Lapeev@wifire.ru>
2025-02-27 23:46:36 +0300

    feat: add RetrieveTimestamp request/response

---
+++ CrmNx.Xrm.Toolkit/Messages/RetrieveTimestampResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CrmNx.Xrm.Toolkit.Messages;
+
+public sealed class RetrieveTimestampResponse
+{
+    /// <summary>
+    /// The time stamp of the metadata
+    /// </summary>
+    public string Timestamp { get; set; }
+}
