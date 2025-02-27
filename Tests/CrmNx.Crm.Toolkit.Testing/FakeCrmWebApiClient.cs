5aa433ba6f54412f8b7813a570d5e4afb2c93dcd
Konstantin Lapeev <konstantin.Lapeev@wifire.ru>
2025-02-27 12:28:43 +0300

    (test): refactor FakeCrmWebApiClient

--- Tests/CrmNx.Crm.Toolkit.Testing/FakeCrmWebApiClient.cs
+++ Tests/CrmNx.Crm.Toolkit.Testing/FakeCrmWebApiClient.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Net.Http;
+using CrmNx.Xrm.Toolkit;
 using CrmNx.Xrm.Toolkit.Infrastructure;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
 
 namespace CrmNx.Crm.Toolkit.Testing
 {
     public class FakeCrmWebApiClient : CrmWebApiClient
     {
-        public string BaseAddress
-        {
-            get => HttpClient.BaseAddress.ToString();
-            set => HttpClient.BaseAddress = new Uri(value);
-        }
-
-        public FakeCrmWebApiClient(HttpClient httpClient, IWebApiMetadataService metadata)
-            : base(httpClient, metadata, NullLogger<FakeCrmWebApiClient>.Instance)
+        public FakeCrmWebApiClient(HttpClient httpClient, IWebApiMetadataService metadata, IOptions<CrmClientSettings> options)
+            : base(httpClient, metadata, options, NullLogger<FakeCrmWebApiClient>.Instance)
         {
         }
 
@@ -25,10 +21,17 @@ namespace CrmNx.Crm.Toolkit.Testing
         /// <returns></returns>
         public static FakeCrmWebApiClient Create(HttpClient httpClient)
         {
+            string connectionString = "Url=http://host.local/demo;Username=;Password=;Domain=;Authtype=AD";
+            
             var metadata = MockedWebApiMetadata.CreateD365Ce();
-            return new FakeCrmWebApiClient(httpClient, metadata)
+            var options = Options.Create(new CrmClientSettings()
             {
-                BaseAddress = SetupBase.D365CeHttpClientBaseAddress.ToString()
+                ConnectionString = connectionString
+            });
+                
+            return new FakeCrmWebApiClient(httpClient, metadata, options)
+            {
+                // BaseAddress = SetupBase.D365CeHttpClientBaseAddress.ToString()
             };
         }
     }
